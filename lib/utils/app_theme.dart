f7373e2e4156085b8a88a0f9861d147a53622577
JEISONVARGAS1 <jeisonvargas@flutterlabapps.com>
2024-12-21 17:43:50 +1300

    init project

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -4,19 +4,21 @@ import 'package:my_fit_ui_kit/utils/ui_color.dart';
 class AppThere {
   ThemeData getThemeData() => ThemeData(
         inputDecorationTheme: InputDecorationTheme(
-          labelStyle: TextStyle(color: UiColor().textColor),
+          labelStyle: TextStyle(color: UiColor().card),
           enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: UiColor().textColor),
+            borderSide: BorderSide(color: UiColor().card),
             borderRadius: BorderRadius.circular(7.0),
           ),
           border: OutlineInputBorder(
-            borderSide: BorderSide(color: UiColor().textColor),
+            borderSide: BorderSide(color: UiColor().card),
             borderRadius: BorderRadius.circular(7.0),
           ),
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(7.0),
-            borderSide: BorderSide(color: UiColor().textColor),
+            borderSide: BorderSide(color: UiColor().card),
           ),
+          fillColor: UiColor().card,
+          filled: true,
         ),
         dividerTheme: DividerThemeData(
           color: UiColor().backgroundButton,
