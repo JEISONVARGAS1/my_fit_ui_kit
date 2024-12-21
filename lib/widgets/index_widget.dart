d939bb74b28d5f8eb9dddeaed279af7c965c589a
JEISONVARGAS1 <jeisonvargas@flutterlabapps.com>
2024-12-21 16:52:32 +1300

    init project

--- lib/widgets/index_widget.dart
+++ lib/widgets/index_widget.dart
@@ -1,13 +1,11 @@
-import 'package:my_fit_ui_kit/widgets/button_widget.dart';
 import 'package:my_fit_ui_kit/widgets/card_widget.dart';
-import 'package:my_fit_ui_kit/widgets/custom_animate_container.dart';
 import 'package:my_fit_ui_kit/widgets/form_widget.dart';
-import 'package:my_fit_ui_kit/widgets/shower_product.dart';
+import 'package:my_fit_ui_kit/widgets/button_widget.dart';
+import 'package:my_fit_ui_kit/widgets/custom_animate_container.dart';
 
 class UiWidget {
   UiForm form = UiForm();
   UiCard card = UiCard();
   Animate animate = Animate();
   UiButton button = UiButton();
-  CompositeWidgets compositeWidgets = CompositeWidgets();
 }
