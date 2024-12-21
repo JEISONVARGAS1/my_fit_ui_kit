6e5c3ac79b975fad6e9f98d38c12d9d10592c739
JEISONVARGAS1 <jeisonvargas@flutterlabapps.com>
2024-12-21 23:44:18 +1300

    init project

---
+++ lib/models/card_type.dart
@@ -0,0 +1,1 @@
+enum CartType{ green, gray }
--- lib/widgets/card_widget.dart
+++ lib/widgets/card_widget.dart
@@ -1,67 +1,95 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:my_fit_ui_kit/utils/ui_color.dart';
+import 'package:my_fit_ui_kit/utils/text_style.dart';
+import 'package:my_fit_ui_kit/models/card_type.dart';
 
 class UiCard {
   Widget simpleCard({
-    double? paddingTop,
-    double? paddingLeft,
-    double? paddingRight,
-    double? paddingBottom,
-    double? horizontalMargin,
-    required List<Widget> body,
-    CrossAxisAlignment? alignment,
+    required String title,
+    required String image,
+    required CartType cartType,
+    required String description,
+    TextStyle? descriptionStyle,
   }) =>
       _CardWidget(
-        body: body,
-        alignment: alignment,
-        paddingTop: paddingTop,
-        paddingLeft: paddingLeft,
-        paddingRight: paddingRight,
-        paddingBottom: paddingBottom,
-        horizontalMargin: horizontalMargin,
+        title: title,
+        image: image,
+        cartType: cartType,
+        description: description,
+        descriptionStyle: descriptionStyle,
       );
 }
 
 class _CardWidget extends StatelessWidget {
-  final List<Widget> body;
-  final double? paddingTop;
-  final double? paddingLeft;
-  final double? paddingRight;
-  final double? paddingBottom;
-  final double? horizontalMargin;
-  final CrossAxisAlignment? alignment;
+  final String title;
+  final String image;
+  final CartType cartType;
+  final String description;
+  final TextStyle? descriptionStyle;
 
   const _CardWidget({
-    this.alignment,
-    this.paddingTop,
-    this.paddingLeft,
-    this.paddingRight,
-    this.paddingBottom,
-    this.horizontalMargin,
-    required this.body,
+    required this.title,
+    required this.image,
+    this.descriptionStyle,
+    required this.cartType,
+    required this.description,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: UiColor().primary,
-        borderRadius: BorderRadius.circular(25),
-      ),
-      alignment: Alignment.center,
-      padding: EdgeInsets.only(
-        top: paddingTop ?? MediaQuery.of(context).size.height * 0.1,
-        left: paddingLeft ?? MediaQuery.of(context).size.width * 0.08,
-        right: paddingRight ?? MediaQuery.of(context).size.width * 0.08,
-        bottom: paddingBottom ?? MediaQuery.of(context).size.height * 0.05,
-      ),
-      margin: EdgeInsets.symmetric(
-        horizontal: horizontalMargin ?? MediaQuery.of(context).size.width * 0.05,
-      ),
-      child: Column(
-        crossAxisAlignment: alignment ?? CrossAxisAlignment.center,
-        children: body,
+    return Material(
+      borderRadius: BorderRadius.circular(20),
+      color: cartType == CartType.green ? UiColor().primary : UiColor().card,
+      child: InkWell(
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: FitTextStyle().title.copyWith(
+                      color: cartType == CartType.green
+                          ? UiColor().backgroundButton
+                          : UiColor().textColor,
+                      shadows: [
+                        const Shadow(
+                          blurRadius: 5.0,
+                          offset: Offset(2, 2),
+                          color: Colors.black26,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                  Text(
+                    description,
+                    style: descriptionStyle ??
+                        FitTextStyle().text.copyWith(
+                          color: cartType == CartType.green
+                              ? UiColor().backgroundButton
+                              : UiColor().textColor,
+                          shadows: [
+                            const Shadow(
+                              blurRadius: 5.0,
+                              offset: Offset(2, 2),
+                              color: Colors.black26,
+                            ),
+                          ],
+                        ),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              right: 0,
+              child: Image(image: AssetImage(image), height: 130),
+            )
+          ],
+        ),
       ),
     );
   }
